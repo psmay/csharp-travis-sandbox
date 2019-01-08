0573e5250fca49edbca0c603031cf323bdcb21b1
Peter S. May <me@psmay.com>
2019-01-08 12:05:34 -0500

    Fix typo

--- scripts/documentation_all.sh
+++ scripts/documentation_all.sh
@@ -4,6 +4,6 @@ export TOP="`pwd`"
 export SCRIPTS="$TOP/scripts"
 export DOCFX_PROJECT_DIR="$TOP/docfx_project"
 export TEMPDIR="`mktemp -d`" &&
-cd "$TEMPDIR"	&&
+cd "$TEMPDIR" &&
 export DOCFX_EXE="`bash "$SCRIPTS/install_docfx_console_here.sh"`" &&
 bash "$SCRIPTS/build_documentation.sh"
