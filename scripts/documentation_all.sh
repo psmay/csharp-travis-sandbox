aad9e57bb06522b33b1caab0d334fae21868a4a3
Peter S. May <me@psmay.com>
2019-01-08 11:54:50 -0500

    Specify bash to avoid need for chmod

--- scripts/documentation_all.sh
+++ scripts/documentation_all.sh
@@ -5,5 +5,5 @@ export SCRIPTS="$TOP/scripts"
 export DOCFX_PROJECT_DIR="$TOP/docfx_project"
 export TEMPDIR="`mktemp -d`" &&
 cd "$TEMPDIR"	&&
-export DOCFX_EXE="`"$SCRIPTS/install_docfx_console_here.sh"`" &&
-"$SCRIPTS/build_documentation.sh"
+export DOCFX_EXE="`bash "$SCRIPTS/install_docfx_console_here.sh"`" &&
+bash "$SCRIPTS/build_documentation.sh"
