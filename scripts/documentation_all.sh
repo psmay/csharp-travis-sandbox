e5cb5dbf2619fc21cc421c2dc11765a8d0dca887
psmay <me@psmay.com>
2019-01-09 13:53:50 -0500

    Provide script functions, including a function to determine the value of a boolean shell variable and a thing that detects the presence of mono before trying to install it. (#5)

--- scripts/documentation_all.sh
+++ scripts/documentation_all.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-export TOP="`pwd`"
-export SCRIPTS="$TOP/scripts"
-export DOCFX_PROJECT_DIR="$TOP/docfx_project"
+. scripts/functions.bash
 
-bash "$SCRIPTS/install_mono_and_nuget.sh" &&
-export TEMPDIR="`mktemp -d`" &&
-cd "$TEMPDIR" &&
-export DOCFX_EXE="`bash "$SCRIPTS/install_docfx_console_here.sh"`" &&
+require_variable TOP
+require_variable SCRIPTS
+default_variable DOCFX_PROJECT_DIR docfx_project
+export DOCFX_PROJECT_DIR="$(realpath "$DOCFX_PROJECT_DIR")"
+export INSTALLS="`mktemp -d`"
+
+install_mono &&
+export DOCFX_EXE=`(
+	cd "$INSTALLS" &&
+	bash "$SCRIPTS/install_docfx_console_here.sh"
+)` &&
 bash "$SCRIPTS/build_documentation.sh" &&
 bash "$SCRIPTS/push_documentation.sh"
