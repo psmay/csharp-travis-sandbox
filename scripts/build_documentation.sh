fef4b98263fcd1801b8b88046aafd96ae4f4fa77
Peter S. May <me@psmay.com>
2019-01-09 13:42:39 -0500

    Provide script functions, including a function to determine the value of a boolean shell variable and a thing that detects the presence of mono before trying to install it.

--- scripts/build_documentation.sh
+++ scripts/build_documentation.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
+. scripts/functions.bash
+
 # Set DOCFX_EXE to the output of install_docfx_console_here.sh
 # Set DOCFX_PROJECT_DIR to the absolute path containing docfx.json
 
-if [ "-$DOCFX_EXE-" = "--" ]; then
-	echo DOCFX_EXE is not set. >&2
-	exit 2
-fi
-
-if [ "-$DOCFX_PROJECT_DIR-" = "--" ]; then
-	echo DOCFX_PROJECT_DIR is not set. >&2
-	exit 3
-fi
+require_variable DOCFX_EXE
+require_variable DOCFX_PROJECT_DIR
 
 cd "$DOCFX_PROJECT_DIR" &&
 mono "$DOCFX_EXE" metadata docfx.json &&
--- scripts/push_documentation.sh
+++ scripts/push_documentation.sh
@@ -1,66 +1,53 @@
 #!/bin/bash
 
-if [ "-$GITHUB_DOC_REPO_OWNER-" = "--" ]; then
-	echo GITHUB_DOC_REPO_OWNER is not set. >&2
-	exit 2
-fi
+. scripts/functions.bash
 
-if [ "-$GITHUB_DOC_REPO-" = "--" ]; then
-	echo GITHUB_DOC_REPO is not set. >&2
-	exit 3
-fi
-
-if [ "-$GITHUB_DOC_GENERATION_TOKEN-" = "--" ]; then
-	echo GITHUB_DOC_GENERATION_TOKEN is not set. >&2
-	exit 4
-fi
-
-if [ "-$DOCFX_PROJECT_DIR-" = "--" ]; then
-	echo DOCFX_PROJECT_DIR is not set. >&2
-	exit 5
-fi
-
-if [ "-$GITHUB_DOC_COMMIT_USER-" = "--" ]; then
-	export GITHUB_DOC_COMMIT_USER="travis_ci_build"
-fi
-if [ "-$GITHUB_DOC_COMMIT_EMAIL-" = "--" ]; then
-	export GITHUB_DOC_COMMIT_EMAIL="travis_ci_build@invalid.invalid"
-fi
+require_variable GITHUB_DOC_REPO_OWNER
+require_variable GITHUB_DOC_REPO
+require_variable GITHUB_DOC_GENERATION_TOKEN
+require_variable DOCFX_PROJECT_DIR
+default_variable GITHUB_DOC_COMMIT_USER travis_ci_build
+default_variable GITHUB_DOC_COMMIT_EMAIL "travis_ci_build@invalid.invalid"
+default_variable PAGES_BRANCH gh-pages
+default_variable PAGES_REPO_DIR pages_repo
 
 git_commit_if_changed () {
 	# Prevent a failed exit if the repo turns out to be unchanged.
 	# https://stackoverflow.com/questions/8123674/how-to-git-commit-nothing-without-an-error
 	(
 		git diff-index --quiet HEAD &&
-		echo 'No changes detected; skipping commit.' >&2
+		eecho 'No changes detected; skipping commit.'
 	) || (
-		echo 'Changes detected; proceeding with commit.' >&2 &&
+		eecho 'Changes detected; proceeding with commit.' &&
 		git commit "$@"
 	)
 }
 
-PAGES_BRANCH=gh-pages
-PAGES_REPO_DIR=pages_repo
 SITE_DEST_IN_REPO="$PAGES_REPO_DIR/docfx"
 
 GITHUB_DOC_REPO_URL_WITH_TOKEN="https://$GITHUB_DOC_REPO_OWNER:$GITHUB_DOC_GENERATION_TOKEN@github.com/$GITHUB_DOC_REPO_OWNER/$GITHUB_DOC_REPO.git"
 
 DOC_PUSH_TEMP_DIR="`mktemp -d`" &&
-cd "$DOC_PUSH_TEMP_DIR" &&
+(
+	cd "$DOC_PUSH_TEMP_DIR" &&
 
-# Get pages repo and config commit user
-git clone "$GITHUB_DOC_REPO_URL_WITH_TOKEN" -b $PAGES_BRANCH $PAGES_REPO_DIR -q &&
-cd $PAGES_REPO_DIR &&
-git config user.email "$GITHUB_DOC_COMMIT_EMAIL" &&
-git config user.name "$GITHUB_DOC_COMMIT_USER" &&
-cd .. &&
+	# Get pages repo and config commit user
+	git clone "$GITHUB_DOC_REPO_URL_WITH_TOKEN" -b $PAGES_BRANCH $PAGES_REPO_DIR -q &&
+	(
+		cd $PAGES_REPO_DIR &&
+		git config user.email "$GITHUB_DOC_COMMIT_EMAIL" &&
+		git config user.name "$GITHUB_DOC_COMMIT_USER"
+	) &&
 
-# Clobber existing site dir and replace with new one
-rm -rf $SITE_DEST_IN_REPO &&
-cp -a "$DOCFX_PROJECT_DIR/_site" $SITE_DEST_IN_REPO &&
+	# Clobber existing site dir and replace with new one
+	rm -rf $SITE_DEST_IN_REPO &&
+	cp -a "$DOCFX_PROJECT_DIR/_site" $SITE_DEST_IN_REPO &&
 
-# Commit and push
-cd $PAGES_REPO_DIR &&
-git add -A &&
-git_commit_if_changed -m "Update from CI #$TRAVIS_BUILD_NUMBER, branch $TRAVIS_BRANCH" -q &&
-git push origin $PAGES_BRANCH -q
+	# Commit and push
+	(
+		cd $PAGES_REPO_DIR &&
+		git add -A &&
+		git_commit_if_changed -m "Update from CI #$TRAVIS_BUILD_NUMBER, branch $TRAVIS_BRANCH" -q &&
+		git push origin $PAGES_BRANCH -q
+	)
+)
